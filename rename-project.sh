284afa42a21c97e1c5a2a171e033cf4d70310a57
Dashamir Hoxha <dashohoxha@gmail.com>
2014-08-09 15:11:49 +0200

    Improved the script rename-project.sh and updated the installation instructions.

--- rename-project.sh
+++ rename-project.sh
@@ -11,12 +11,12 @@ Usage: $0 from_project:to_project from_prefix:to_prefix
 
 ### rename_files $from $to
 ### The first argument is replaced with the second argument
-### in all file and directory names.
+### in all file and directory names in the current directory.
 function rename_all_files {
     from=$1
     to=$2
 
-    files=$(find $to_project/ -name "*$from*")
+    files=$(find . -name "*$from*")
     while test -n "$files"
     do
 	for file in $files
@@ -24,18 +24,19 @@ function rename_all_files {
 	    file1=${file/$from/$to}
             mv $file $file1 2> /dev/null
 	done
-	files=$(find $to_project/ -name "*$from*")
+	files=$(find . -name "*$from*")
     done
 }
 
 
+### replace_in_all_files $from $to
 ### Replace the first argument with the second argument
-### in all the files of the project.
+### in all the files of the currect directory.
 function replace_in_all_files {
     from=$1
     to=$2
 
-    for file in $(find $to_project/ -type f)
+    for file in $(find . -type f)
     do
         sed -i $file -e "s/$from/$to/g"
     done
@@ -54,19 +55,11 @@ then
     usage
 fi
 
-### Rename.
-cp -a $from_project $to_project
+### Rename and replace all files in the script directory.
+cd $(dirname $0)
+rm -rf .git
 rename_all_files $from_project $to_project
 rename_all_files $from_prefix $to_prefix
 replace_in_all_files $from_project $to_project
 replace_in_all_files ${from_project} ${to_project^}
 replace_in_all_files $from_prefix $to_prefix
-
-### Commit changes to git.
-cd $to_project/
-rm -rf .git
-cp -a ../$from_project/.git .
-git add $(git ls-files --others)
-git add $(git ls-files --modified)
-git rm  $(git ls-files --deleted)
-git commit -m "Renamed project from $from_project/$from_prefix to $to_project/$to_prefix."
